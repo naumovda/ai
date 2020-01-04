aed71697072ed406222756e9e1767da625a3d7df
Naumov Dmitry <lonlymage@mail.ru>
2020-01-04 10:21:20 +0300

    Modified evaluator functions for A*Search

--- code/eights/asearch.py
+++ code/eights/asearch.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue
-from eight_list import state 
+from eight_parent import state 
 from timeit import Timer
 
 # Fair Evaluator ::= P(h)
@@ -15,12 +15,12 @@ def fair_evaluator(state, goal):
             dst[goal._data[row*size+col]] = (row, col)
 
     dist = 0
-    for idx in range(len(src)):
+    for idx in range(1, len(src)):
         dist += abs(src[idx][0]-dst[idx][0]) + abs(src[idx][1]-dst[idx][1])
 
     return dist
 
-# Good Evaluator ::= P(h)+3-S(h)
+# Good Evaluator ::= P(h)+3*S(h)
 #   P(h) - сумма манхеттенских расстояний между всеми ячейками
 #   S(h) - для каждой плитки, 
 #             0 - если за ней идет корректный приемник
@@ -30,8 +30,12 @@ def good_evaluator(state, goal):
     cells = [(0,1), (1, 2), (2, 5), (5, 8), (8, 7), (7, 6), (6, 3), (3, 0)]
     s = 0
     for first, second in cells:
-        if state._data[first] + 1 != state._data[second]:
-            s += 2
+        if state._data[first] == 8:
+            if state._data[second] != 1:
+                s += 2
+        elif state._data[first] != 0:
+            if state._data[first] + 1 != state._data[second]:
+                s += 2
     if state._data[4] != state.space:
         s += 1
     return fair_evaluator(state, goal) + 3*s
@@ -41,22 +45,28 @@ def good_evaluator(state, goal):
 def weak_evaluator(state, goal):
     count = 0
     for k in range(len(state._data)):
-        if state._data[k] != goal._data[k]:
+        if state._data[k] != goal._data[k] and state._data[k] != 0:
             count += 1
     return count 
 
 # Bad Evaluator ::= |D(h) - 16|
-#   D(h) - сумма модулей разности значений противоположных (относительно центра) плиток для текущего состояния
-#   G(h) - сумма модулей разности значений противоположных (относительно центра) плиток для целевого состояния      
+#   D(h) - сумма разностей значений противоположных (относительно центра) плиток для текущего состояния
+#   G(h) - сумма разностей значений противоположных (относительно центра) плиток для целевого состояния      
 def bad_evaluator(state, goal):
-    d = abs(state._data[0] - state._data[8]) \
-        + abs(state._data[1] - state._data[7]) \
-        + abs(state._data[2] - state._data[6]) \
-        + abs(state._data[3] - state._data[5])
-    g = abs(goal._data[0] - goal._data[8]) \
-        + abs(goal._data[1] - goal._data[7]) \
-        + abs(goal._data[2] - goal._data[8]) \
-        + abs(goal._data[3] - goal._data[5])
+    d = state._data[3] - state._data[5] \
+        + state._data[6] - state._data[2] \
+        + state._data[7] - state._data[1] \
+        + state._data[8] - state._data[0]
+    g = goal._data[3] - goal._data[5] \
+        + goal._data[6] - goal._data[2] \
+        + goal._data[7] - goal._data[1] \
+        + goal._data[8] - goal._data[0]
+    # print(d)
+    # print(g)
+    # sumDistance += node.cell(1, 0) - node.cell(1, 2); 3 - 5
+	# sumDistance += node.cell(2, 0) - node.cell(0, 2); 6 - 2
+	# sumDistance += node.cell(2, 1) - node.cell(0, 1); 7 - 1
+	# sumDistance += node.cell(2, 2) - node.cell(0, 0); 8 - 0
     return abs(d-g)
 
 def a_search(initial, goal, evaluator):
@@ -87,14 +97,21 @@ def a_search(initial, goal, evaluator):
 
     while not open_states.empty():
         # извлекаем первый элемент из (очереди) списка открытых состояний
-        _, current = open_states.get()
+        value, current = open_states.get()
+
+        print(current, ' v = ', value, 'd = ', current._depth)
 
         # добавляем его в закрытые
         closed_states.add(current)
 
         if current == goal:
             # сформировать список ходов до текущего
-            path = [current]
+            item = current
+            path = [item]
+            while item._parent != None:
+                item = item._parent
+                path.append(item)                
+            path.reverse()
             return {'solved':True, 
                 'path': path, 
                 'openstates':len(open_states.queue), 
@@ -125,16 +142,31 @@ def a_search(initial, goal, evaluator):
         }
 
 if __name__ == '__main__':
-    initial = state([2,1,6,4,0,8,7,5,3], 4, 0)
-    goal = state([1,2,3,8,0,4,7,6,5], 4)
+    # initial = state(None, [2,1,6,4,0,8,7,5,3], 4, 0)
+    # initial = state(None, [1,4,8,7,3,0,6,5,2], 5, 0)
+    initial = state(None, [8,1,3,0,4,5,2,7,6], 3, 0)
+    goal = state(None, [1,2,3,8,0,4,7,6,5], 4)
+
+    # 1,4,8
+    # 7,3,0
+    # 6,5,2
+    # 1 +2
+    # 4 +2
+    # 8 +0
+    # 
+
+    print('fair =', fair_evaluator(initial, goal))
+    print('good =', good_evaluator(initial, goal))    
+    print('weak =', weak_evaluator(initial, goal))
+    print('bad  =', bad_evaluator(initial, goal))
 
     # fair_evaluator
     # good_evaluator
     # weak_evaluator
     # bad_evaluator
-    f = lambda: a_search(initial, goal, fair_evaluator)
+    # f = lambda: a_search(initial, goal, fair_evaluator)
     # f = lambda: a_search(initial, goal, good_evaluator)
-    # f = lambda: a_search(initial, goal, weak_evaluator)
+    f = lambda: a_search(initial, goal, weak_evaluator)
     # f = lambda: a_search(initial, goal, bad_evaluator)
 
     res = f()        
--- code/eights/test.py
+++ code/eights/test.py
@@ -7,5 +7,10 @@ from eight_dict import state
 if __name__ == "__main__":
     initial = state([[8,1,3],[2,4,5],[state.space,7,6]], 0)
     
+    for idx in range(1, 5):
+        print(idx)
+
     print(initial)
     print(initial.__hash__())
+
+    print(1*2*3*4*5*6*7*8*9)
