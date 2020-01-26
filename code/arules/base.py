c1a46cb27f75775cfcc15b559bb012670b2457bd
nd <lonlymage@mail.ru>
2020-01-26 20:48:50 +0300

    Continue Apriori

--- code/arules/base.py
+++ code/arules/base.py
@@ -1,69 +1,50 @@
-class itemset:
-    '''
-    Элемент
-        Атрибуты:
-            items - множество элементов, входящих в набор
-        Методы:
-            support - поддержка набора в базе данных 
-    '''
-    def __init__(self, itemset=set()):
-        self.itemset = itemset
-        self.count = 0
-        self.total = 0
+# class itemset:
+#     '''
+#     Элемент
+#         Атрибуты:
+#             items - множество элементов, входящих в набор
+#         Методы:
+#             support - поддержка набора в базе данных 
+#     '''
+#     def __init__(self, itemset=set()):
+#         self.itemset = itemset
+#         self.count = 0
+#         self.total = 0
 
-    def add(self, value):
-        self.itemset.add(value)
+#     def add(self, value):
+#         self.itemset.add(value)
 
-    def union(self, other):
-        result = set()
-        result.update(self)
-        result.update(other)
-        return result
+#     def union(self, other):
+#         result = set()
+#         result.update(self)
+#         result.update(other)
+#         return result
 
-    def support(self):
-        if self.total == 0:
-            return 0        
-        return self.count/self.total
+#     def support(self):
+#         if self.total == 0:
+#             return 0        
+#         return self.count/self.total
 
-    def __eq__(self, value):
-        return self.itemset == value.itemset
+#     def __eq__(self, value):
+#         return self.itemset == value.itemset
 
-    def __str__(self):
-        return f"set={self.itemset}, support={self.support()}"
+#     def __str__(self):
+#         return f"set={self.itemset}, support={self.support()}"
 
 class transaction:
     '''
     Транзакция, имеющая идентификатор и содержащая набор элементов 
     '''
-    cid = -1
+    def __init__(self, tid, itemset=frozenset()):
+        self.tid = tid
+        self.itemset = itemset
 
-    def __init__(self, itemset=set()):
-        transaction.cid += 1
-        self.tid = transaction.cid
-        self.itemset = set()
-        self.itemset.update(itemset)
+    def get_list(self, fields):
+        return sorted(list(self.itemset))
 
-    def add(self, item):
-        self.itemset.add(item)
-    
-    def update(self, items):
-        self.itemset.update(items)
-
-    def to_list(self, fields):
-        items = []
-        for item in fields:
-            if item in self.itemset:
-                items.append(item)
-        return items
-
-    def to_boolean_list(self, fields):
-        items = []
-        for item in fields:
-            if item in self.itemset:
-                items.append('1')
-            else:
-                items.append('0')
-        return items
+    def get_boolean(self, fields):
+        digit = lambda value: "1" if value in self.itemset else "0"
+        return [digit(item) for item in fields]
 
 class database:
     '''
@@ -76,110 +57,119 @@ class database:
     def add_transaction(self, item):
         self.transactions.append(item)
 
-    def calc_support(self, itemsets):
-        total = len(self.transactions)   
-        for itemset in itemsets:
-            itemset.count = 0
-            itemset.total = total
-        for transaction in self.transactions:
-            for itemset in itemsets: 
-                if itemset.itemset <= transaction.itemset:
-                    itemset.count += 1
-
-    def print_set(self):
+    def print_as_set(self):
         for t in self.transactions:
-            print(t.tid, " ".join(t.to_list(self.fields)))
+            print(t.tid, " ".join(t.get_list(self.fields)))
         print()
 
-    def print_boolean(self):
+    def print_as_boolean(self):
         print("#", " ".join(self.fields))
         for t in self.transactions:
-            print(t.tid, " ".join(t.to_boolean_list(self.fields)))
+            print(t.tid, " ".join(t.get_boolean(self.fields)))
         print()
 
     def load(self, file_name):
         pass
 
-class apriori:
-    def __init__(self, min_support):
-        self.min_support = min_support
-        self.itemsets = []
+def calc_support(database, itemsets, support):
+    count = {itemset:0 for itemset in itemsets}
+    for transaction in database.transactions:
+        for itemset in itemsets: 
+            if itemset.itemset <= transaction.itemset:
+                count[itemset] += 1
+    total = len(database.transactions)   
+    for itemset in itemsets: 
+        support[itemset] = count[itemset] / total       
 
-    def append_itemsets(self, items):
+def subsets(source):
+    result = []
+    for item in source:
+        temp = source.copy()
+        temp.remove(item)
+        if temp != []:
+            result.append(temp)
+            for elem in subsets(temp):
+                if not elem in result:
+                    result.append(elem)             
+    return result
+
+class apriori:
+    def __init__(self, min_support, min_confidence):
+        self.min_support = min_support
+        self.min_confidence = min_confidence
+        self.itemsets = []
+        self.support = {}
+
+    def filter(self, items, support):
         # формируем список наборов с поддержкой, большей min_support
-        res = []
-        for item in items:
-            if item.support() >= self.min_support:
-                res.append(item)
-        self.itemsets.append(res)
+        return [item for item in items if support[item] >= self.min_support]
 
     def step_0(self, db):
         self.itemsets = []
+        self.support = {}
         # формируем одноэлементные наборы 
-        items = []        
-        for item in db.fields:
-            items.append(itemset(set([item])))        
+        items = [frozenset([item]) for item in db.fields]        
         # рассчтываем поддержку одноэлементных наборов
-        db.calc_support(items)
+        calc_support(db, items, self.support)
         # добавляем в список
-        self.append_itemsets(items)        
+        self.itemsets.append(self.filter(items, self.support))        
         
-    def step_k(self, db):
-        if self.itemsets[-1] == []:
-            return False
-        items = []
-        for e1 in self.itemsets[0]:
-            for e2 in self.itemsets[-1]:
-                if not e1.itemset <= e2.itemset:
-                    s = set()
-                    s.update(e1.itemset)
-                    s.update(e2.itemset)                    
-                    e = itemset(s)
-                    if not e in items:
-                        items.append(e)
-        # рассчтываем поддержку наборов
-        db.calc_support(items)
-        # добавляем в список
-        self.append_itemsets(items)       
-        # возвращаем признак того, что можно продолжать
-        return True
+    # def step_k(self, db):
+    #     if self.itemsets[-1] == []:
+    #         return False
+    #     items = []
+    #     for e1 in self.itemsets[0]:
+    #         for e2 in self.itemsets[-1]:
+    #             if not e1.itemset <= e2.itemset:
+    #                 s = set()
+    #                 s.update(e1.itemset)
+    #                 s.update(e2.itemset)                    
+    #                 e = itemset(s)
+    #                 if not e in items:
+    #                     items.append(e)
+    #     # рассчтываем поддержку наборов
+    #     db.calc_support(items)
+    #     # добавляем в список
+    #     self.append_itemsets(items)       
+    #     # возвращаем признак того, что можно продолжать
+    #     return True
 
-class rule:
-    def __init__(self, antecedent, consequent, support, confidence):
-        self.antecedent = antecedent
-        self.consequent = consequent
-        self.support = support
-        self.confidence = confidence   
+# class rule:
+#     def __init__(self, antecedent, consequent, support, confidence):
+#         self.antecedent = antecedent
+#         self.consequent = consequent
+#         self.support = support
+#         self.confidence = confidence   
    
 if __name__ == "__main__":
     # загружаем файл данных
     db = database(['A','B','C','D','E','F'])
     
-    t1 = transaction(set(['A','B','C']))
-    t2 = transaction(set(['A','C']))
-    t3 = transaction(set(['A','D']))
-    t4 = transaction(set(['B','E','F']))
+    t1 = transaction(0, frozenset(['A','B','C']))
+    t2 = transaction(1, frozenset(['A','C']))
+    t3 = transaction(2, frozenset(['A','D']))
+    t4 = transaction(3, frozenset(['B','E','F']))
 
     db.add_transaction(t1)
     db.add_transaction(t2)
     db.add_transaction(t3)
     db.add_transaction(t4)
 
-    db.print_set()
-    db.print_boolean()
+    db.print_as_set()
+    db.print_as_boolean()
 
-    alg = apriori(0.25)
-    alg.step_0(db)
+    # alg = apriori(0.25)
+    # alg.step_0(db)
 
-    while alg.step_k(db):
-        pass
+    # while alg.step_k(db):
+    #     pass
 
-    k = 0
-    for level in alg.itemsets:        
-        print('level', k)
-        for item in level:
-            print(item)
-        k += 1
+    # k = 0
+    # for level in alg.itemsets:        
+    #     print('level', k)
+    #     for item in level:
+    #         print(item)
+    #     k += 1
     # db.load('mushroom_csv.csv')
     
     # формируем одноэлементные наборы 
--- code/arules/test.py
+++ code/arules/test.py
@@ -2,21 +2,42 @@
 # for key in sorted(d.keys()):
 #     print(key, d[key])
 
-s1 = [1, 2, 3, 4, 5]
+def subsets(source):    
+    result = []
+    for item in source:
+        temp = source.copy()
+        temp.remove(item)
+        if temp != []:
+            result.append(temp)
+            for elem in subsets(temp):
+                if not elem in result:
+                    result.append(elem)             
+    return result
 
-n =2**len(s1)
+d = {}
+l1 = [1, 2, 3, 4, 5]
+s1 = frozenset(l1)
 
-print(n)
+d[s1] = 1
+print(s1, d[s1])
 
-def confidence(rule):
-    return 1.0
+z = {item: 0 for item in l1}
+print(z)
+# print(subsets(s1))
 
-def assoc_rules(rules, items, seq, min_confidence):
-    items = list()
-    seq = list()
-    for item in items:
-        new_items = items.copy()
-        new_items.remove(item)
-        seq.append(item)
-        if confidence(None) >= min_confidence:
+# n =2**len(s1)
+
+# print(n)
+
+# def confidence(rule):
+#     return 1.0
+
+# def assoc_rules(rules, items, seq, min_confidence):
+#     items = list()
+#     seq = list()
+#     for item in items:
+#         new_items = items.copy()
+#         new_items.remove(item)
+#         seq.append(item)
+#         if confidence(None) >= min_confidence:
             
