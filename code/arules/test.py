d62c73a5b40d6f3b6081ec1dfeb494f4bb83c110
Naumov Dmitry <lonlymage@mail.ru>
2020-02-13 06:59:00 +0300

    Added AR lection

--- code/arules/test.py
+++ code/arules/test.py
@@ -1,5 +1,29 @@
-items = [1, 2 , 3, 4]
+# items = [1, 2 , 3, 4]
+# items.sort(key=lambda item: -1 * item)
+# print(items[:0])
 
-items.sort(key=lambda item: -1 * item)
+v1 = {'v':1, 'childs':[]}
+v2 = {'v':2, 'childs':[]}
+v3 = {'v':3, 'childs':[]}
+v4 = {'v':4, 'childs':[]}
+v5 = {'v':5, 'childs':[]}
+v7 = {'v':7, 'childs':[v1, v2, v3]}
+v8 = {'v':8, 'childs':[v4, v5]}
+root = {'v':0, 'childs':[v7, v8]}
 
-print(items[:0])
+#     0
+#   ------ 
+#   7    8
+# ----- ---
+# 1 2 3 4 5
+# 123456789
+
+def width(root):
+    if root['childs'] == []:
+        return 1
+    return sum([width(child) for child in root['childs']])+len(root['childs'])-1
+
+def print_tree(root):    
+    print(f"{root['v']}")
+
+print(width(root))
