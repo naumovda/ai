4a5b7f7fc89edf311e31284d48e7b2be5c50b62e
lonlymage <lonlymage@mail.ru>
2020-09-24 10:47:03 +0300

    Minimax example v0.1

---
+++ code/tictactoe/test.py
@@ -0,0 +1,5 @@
+s = [[None for _ in range(3)] for _ in range(3)]
+
+print(s)
+s[1][0] = "X"
+print(s)
--- code/tictactoe/xo.py
+++ code/tictactoe/xo.py
@@ -1,68 +1,94 @@
-from copy import deepcopy
-
-players = ["X", "0"]
-
-def swap_player(player):
-    return ({"X":"0", "0":"X"})[player]
-
-def get_lines():
-    lines = [
-        [(0,0), (0,1), (0,2)],
-        [(1,0), (1,1), (1,2)],
-        [(2,0), (2,1), (2,2)],
-        [(0,0), (1,0), (2,0)],
-        [(0,1), (1,1), (2,1)],
-        [(0,2), (1,2), (2,2)],
-        [(0,0), (1,1), (2,2)],
-        [(0,2), (1,1), (2,0)],
-    ]
-    return lines
-
 class state:
-    def __init__(self, player, value=None):
-        if not player in players:
-            raise Exception
+    infinity = 100
 
-        self.player = player
+    lines = [
+        [(0,0), (0,1), (0,2)], [(1,0), (1,1), (1,2)],
+        [(2,0), (2,1), (2,2)], [(0,0), (1,0), (2,0)],
+        [(0,1), (1,1), (2,1)], [(0,2), (1,2), (2,2)],
+        [(0,0), (1,1), (2,2)], [(0,2), (1,1), (2,0)]]
 
-        if value != None:
+    players = ["X", "0"]
+    opponent = {"X":"0", "0":"X"}
+
+    def __init__(self, value=None): # player
+        if value:
             self.value = value
         else:
-            self.value = [
-                [None, None, None], 
-                [None, None, None], 
-                [None, None, None]]                
+            self.value = [[None for _ in range(3)] for _ in range(3)]              
 
     def __str__(self):
         return str(self.value)
 
-    def get_moves(self):
-        states = []
+    def get_moves(self, player):
+        moves = []
         for row in range(3):
             for col in range(3):
                 if self.value[row][col] == None:
-                    new_state = deepcopy(self.value)
-                    new_state[row][col] = self.player
-                    states.append(state(swap_player(self.player), new_state))
-        return states    
+                    moves.append((row, col, player))
+        return moves
+
+    def do_move(self, move):
+        row, col, player = move
+        self.value[row][col] = player        
+
+    def undo_move(self, move):
+        row, col, _ = move
+        self.value[row][col] = None
 
     def is_win(self, player):
-        lines = get_lines()    
-        for line in lines:        
+        for line in state.lines:                                
             is_win = True
             for i, j in line:
                 is_win = is_win and (self.value[i][j]==player)
             if is_win:
                 return True
-        return False        
+        return False  
+
+    def nc(self, player):
+        count = 0
+        for (r1,c1),(r2,c2),(r3,c3) in state.lines:
+            if self.value[r1][c1] != state.opponent[player] \
+                and self.value[r2][c2] != state.opponent[player] \
+                and self.value[r3][c3] != state.opponent[player]:
+                count += 1
+        return count
+
+    def score(self, player):
+        oppenent = state.opponent[player]
+        if self.is_win(player):
+            return state.infinity
+        elif self.is_win(oppenent):
+            return (-1)*state.infinity 
+        else:
+            return self.nc(player) - self.nc(oppenent)
+
+def minimax (state, level, original, player, opponent):
+    best_move, best_score = None, None
+    moves = state.get_moves(player)
+
+    if level == 0 or moves == []:
+        return None, state.score(player)
+
+    for m in moves:
+        state.do_move(m)
+        _, score = minimax(state, level-1, original, opponent, player)
+        state.undo_move(m)
+        if player == original:
+            if best_score == None or score > best_score:
+                best_move, best_score =  m, score
+        else:
+            if best_score == None or score < best_score:
+                best_move, best_score =  m, score
+    return best_move, best_score
+
+def bestmove(state, level, player, opponent):
+    return minimax(state, level, player, player, opponent)
 
 if __name__ == "__main__":
-    s = state("X")
-    print(s)
+    s = state()
+    level = 4
+    player, opponent = "X", state.opponent["X"]    
+    
+    move = bestmove(s, level, player, opponent)
 
-    for item in s.get_moves():
-        print(item)
-
-    print(s.get_moves()[0].get_moves()[0])
-
-
+    print(f"Best move is: ", move)
