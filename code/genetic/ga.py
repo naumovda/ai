ed4fe98bca24ff80a28a9b5c167d2fadc47e7e3d
Naumov Dmitry <naumov_rgrtu@mail.ru>
2020-10-25 14:16:33 +0300

    Added GA example

--- code/genetic/ga.py
+++ code/genetic/ga.py
@@ -1,18 +1,27 @@
 from math import sin, pi
+import random 
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Individual:
+class Species:
     MIN = 0
     MAX = 5
     N = 2 ** 16
+    MUT_PROBABILITY = 0.15 # вероятность мутации
 
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, value = None):
+        if value:
+            self.value = value
+        else:
+            self.value = random.randint(Species.MIN, Species.MAX)            
         self.set_gen()
+
+    def __lt__(self, value):
+        return self.fitness() < value.fitness()
 
     def set_gen(self):
         '''Преобразование фенотипа в генотип'''
-        x = int((self.value - Individual.MIN) \
-            / (Individual.MAX - Individual.MIN) * Individual.N)
+        x = int((self.value - Species.MIN) / (Species.MAX - Species.MIN) * Species.N)
         s = ''
         while x > 0:
             s += str(x % 2)
@@ -21,188 +30,150 @@ class Individual:
         for _ in range(16 - len(s)):
             s = '0' + s 
         self.gen = s
-        
-    def set_pheno(self):
-        '''Преобразование генотипа в фенотипа'''
-        pass
+
+    @classmethod    
+    def get_pheno(cls, gen):
+        '''Получение фенотипа из генотипа'''
         t = 0
         p = 1
-        for bit in self.gen:
-            if bit == 1:
+        for bit in gen:
+            if bit == '1':
                 t += p
             p *= 2
-        return Individual.MIN + t*(Individual.MAX-Individual.MIN)\
-            / Individual.N
+        return Species.MIN + t*(Species.MAX-Species.MIN) / Species.N
 
-    # Фитнесс-функция
-    # f(t)=(1.5t+0.9)sin(πt+1.1) 
-    def finness(self):
+    def pheno(self):
+        return Species.get_pheno(self.gen)
+
+    def fitness(self):
+        '''Фитнесс-функция'''
+        # f(t)=(1.5t+0.9)sin(πt+1.1) 
         return (1.5 * self.value + 0.9)*sin(pi*self.value + 1.1)
 
-# const
-#   Pm = 4e-1; //вероятность мутации
-#   RUN_COUNT = 20; //количество поколений
-#   P_COUNT = 20; //кол-во особей в популяции
+    @staticmethod
+    def inverse(bit):
+        return '1' if bit == '0' else '0'
 
-# type
-#   //сама популяция
-#   TPopulation = array[1..2*P_COUNT] of string;
+    # Функция мутации
+    def mutation(self):
+        if random.random() < Species.MUT_PROBABILITY:
+            mutation_index = random.randint(0, len(self.gen)-1)      
+            new_gen = self.gen[:mutation_index] + Species.inverse(self.gen[mutation_index]) + self.gen[mutation_index+1:]
+            new_species = Species(Species.get_pheno(new_gen))
+            return new_species
+        else:
+            return None
 
+class Population:
+    SPC_COUNT = 40 # кол-во особей в популяции
 
-# function Mutation(s: string): string;
-# var
-#   n: integer;
-#   s1: string;
-# begin
-#   s1 := s;
+    def __init__(self):
+        self.species = [Species() for _ in range(Population.SPC_COUNT+1)]
 
-#   //номер гена для мутации
-#   n := round(length(s)*random)+1;
+    def max_fitness(self):
+        '''определение максимума функции приспособленности'''
+        return max([item.fitness() for item in self.species])
 
-#   if s1[n] = '1' then
-#     s1[n] := '0'
-#   else
-#     s1[n] := '1';
+    def avg_fitness(self):
+        '''определение среднего функции приспособленности'''
+        return sum([item.fitness() for item in self.species]) / len(self.species)
 
-#   Mutation := s1;
-# end;
+    def __str__(self):
+        return f"max = {self.max_fitness():.2f} avg = {self.avg_fitness():.2f}"
 
+    def mutation(self):
+        '''добавление в популяцию особей с мутацией'''
+        new_population = []
+        for species in self.species:
+            new_species = species.mutation()
+            if new_species:
+                new_population.append(new_species)
+        self.species.extend(new_population)
 
-# //определение макисмума функции приспособленности
-# function MaxF(var P:TPopulation):real;
-# var
-#   i: integer;
-#   tmax: real;
-# begin
-#   tmax := f(GenToPheno(P[1], MIN, MAX));
+    def selection(self):        
+        self.species.sort(reverse=True) # сортируем популяцию b         
+        self.species = self.species[:Population.SPC_COUNT+1] # отбираем только лучшие SPC_COUNT особей
 
-#   for i := 2 to P_COUNT do
-#     if f(GenToPheno(P[i], MIN, MAX)) > tmax then
-#       tmax := f(GenToPheno(P[i], MIN, MAX));
+class Evolution: 
+    def __init__(self, gen_count):        
+        self.gen_count = gen_count
+        self.generation = None
+        self.population = None
+        self.max = []
+        self.avg = []
 
-#   Result := tmax;
-# end;
+    def add_statistic(self):
+        self.max.append(self.max_fitness())  
+        self.avg.append(self.avg_fitness())  
 
-# //определение среднего функции приспособленности
-# function AvgF(var P:TPopulation):real;
-# var
-#   i: integer;
-#   tavg: real;
-# begin
-#   tavg := 0;
+    def start(self):        
+        self.max = []
+        self.avg = []        
+        self.generation = 0
+        self.population = Population()       
+        self.add_statistic()
 
-#   for i := 1 to P_COUNT do
-#      tavg := tavg + f(GenToPheno(P[i], MIN, MAX));
+    def next(self):
+        self.generation += 1
+        # применяем оператор мутации
+        self.population.mutation()        
+        # выполняем xотбор
+        self.population.selection()
+        self.add_statistic()
 
-#   Result := tavg / P_COUNT;
-# end;
+    def run(self, debug=False):
+        self.start()
+        if debug: print(f"start generation: {self.population}")      
 
-# //вывод популяции
-# procedure WriteP(var P:TPopulation; Count: integer);
-# var
-#   i: integer;
-# begin
-#   writeln('Population:');
-#   for i := 1 to Count do
-#   begin
-#     write('i = ');
-#     write(P[i], ' ');
-#     write(GenToPheno(P[i], MIN, MAX):0:4, ' ');
-#     write(f(GenToPheno(P[i], MIN, MAX)):0:4, ' ');
-#     writeln;
-#   end;
-#   writeln;
-# end;
+        for k in range(self.gen_count):            
+            self.next()
+            if debug: print(f"generation {self.generation}: {self.population}")
 
-# var
-#   //популяция
-#   Population: TPopulation;
+    def max_fitness(self):
+        return self.population.max_fitness()
 
-#   //новое поколение + старое
-#   NewPopulation: TPopulation;
+    def avg_fitness(self):
+        return self.population.avg_fitness()
 
-#   //всего новых особей
-#   NewCount: integer;
+def test_runonce():
+    gen_count = 50
+    e = Evolution(gen_count)
+    e.run(debug=True)
+    print(f'After {gen_count} generation:')
+    print(e.population)
+    print(f'The best species is {e.population.species[0].value} ({e.population.species[0].gen}) with fitness: {e.population.species[0].fitness():.2f}')
 
-#   i, j, p, k: integer;
-#   x, rnd: real;
+def test_function():
+    fitness = lambda x: (1.5*x+0.9)*sin(pi*x+1.1)
+    x = np.arange(0, 5, 0.1)
+    y = np.array([fitness(elem) for elem in x])
+    _ = plt.figure()
+    plt.plot(x, y)
+    plt.title('Fitness functions')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show() 
 
-#   tmp_s: string;
-#   f1, f2: real;
+def test_picture():
+    gen_count = 140
+    e = Evolution(gen_count)
+    e.run()     
+    
+    x = np.arange(0, gen_count+1, 1)    
+    y1 = np.array([int(elem) for elem in e.avg])
+    y2 = np.array([int(elem) for elem in e.max])
 
-# begin
-#   //PhenoToGen(1, 0, 5);
-#   //writeln(GenToPheno('0011001100110011', 0, 5));
-#   randomize;
-
-#   //формируем начальную популяцию
-#   for i := 1 to P_COUNT do
-#   begin
-#     x := random * (MAX-MIN) + MIN;
-
-#     //получаем генотип, сохраняем в Population
-#     Population[i] := PhenoToGen(x, MIN, MAX);
-#   end;
-
-#   writeln('START');
-#   writeln('Max fitness = ', MaxF(Population));
-#   writeln('Avg fitness = ', AvgF(Population));
-
-#   // запуск эволюции!
-#   for k := 1 to RUN_COUNT do
-#   begin
-#     WriteP(Population, P_COUNT);
-
-#     //применяем оператор мутации
-#     NewCount := 0;
-#     for i := 1 to P_COUNT do
-#     begin
-#       if random < Pm then //если мутация сработала
-#       begin
-#         //добавляем в NewPopulation
-#         NewCount := NewCount + 1;
-
-#         NewPopulation[NewCount] := Mutation(Population[i]);
-#       end;
-#     end;
-
-#     //добавляем старое поколение
-#     for i := 1 to P_COUNT do
-#     begin
-#       NewCount := NewCount + 1;
-#       NewPopulation[NewCount] := Population[i];
-#     end;
-
-#     WriteP(NewPopulation, NewCount);
-
-#     //сортируем популяцию
-#     for i := 1 to NewCount-1 do
-#       for j := i+1 to NewCount do
-#       begin
-#         f1 := f(GenToPheno(NewPopulation[i], MIN, MAX));
-#         f2 := f(GenToPheno(NewPopulation[j], MIN, MAX));
-
-#         if f1 < f2 then
-#         begin
-#           tmp_s := NewPopulation[i];
-#           NewPopulation[i] := NewPopulation[j];
-#           NewPopulation[j] := tmp_s;
-#         end;
-#       end;
-
-#     //отбираем только лучшие P_COUNT особей
-#     for i := 1 to P_COUNT do
-#       Population[i] := NewPopulation[i];
-
-#     //выводим статистику
-#     writeln('Generation ', k);
-#     writeln('Max fitness = ', MaxF(Population));
-#     writeln('Avg fitness = ', AvgF(Population));
-#   end;
-
-#   writeln('Press enter');
-#   readln;
-# end.
-
-
-
+    plt.title('Fitness')
+    plt.plot(x, y1, 'r', x, y2, 'g') 
+    plt.xlabel("generations")
+    plt.ylabel("fitness")
+    plt.title("Fitness(generation number)")
+    plt.legend(['AVG fitness', 'MAX fitness'])
+    plt.grid()
+    plt.show()    
+   
+if __name__ == "__main__":
+    # test_function()
+    # test_runonce()
+    test_picture()
