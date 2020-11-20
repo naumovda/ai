a971396c7b803d969c131f8f26de58d503eb4256
lonlymage <lonlymage@mail.ru>
2020-11-20 13:43:02 +0300

    Added colored map lab

---
+++ code/2check/choropleth/bfs.py
@@ -0,0 +1,56 @@
+def bfs(initial, goal):
+    """breadth search function
+
+    Keyword arguments:
+    initial -- inital state, start of the search tree
+    goal -- final state, we want to find path
+
+    Return: a tuple (is_find, path, open_state_count, close_state_count)
+    is_find -- does path has been found
+    path -- a path to final state, a solution
+    open_state_count -- count of elements in open states list
+    close_state_count -- count of elements in close states list
+    """
+    if initial.is_goal():  # начальное состояние равно целевому
+        return (True, initial, 0, 0)
+
+    # список открытых состояний
+    initial._depth = 0
+    open_states = [initial]
+
+    # список закрытых состояний
+    closed_states = []
+
+    while open_states != []:
+        # извлекаем первый элемент из (очереди) списка открытых состояний
+        current = open_states.pop(0)
+
+        print(f'd={current._depth}')
+
+        # добавляем его в закрытые
+        closed_states.append(current)
+
+        # генерируем список возможных ходов
+        for m in current.get_moves():
+            # get_moves() это делает, но вдруг забыли реализовать
+            m._depth = current._depth + 1
+
+            # если этот ход не в списке закрытых состояний
+            if m not in closed_states:
+                if m.is_goal():
+                    # сформировать список ходов до текущего
+                    return (True, m, len(open_states), len(closed_states))
+                open_states.append(m)
+    return (False, None, -1, -1)  # нет решения, возвращаем пустой список
+
+if __name__ == '__main__':
+    from state import MapState
+    import usa_graph
+
+    initial = MapState(usa_graph.TEST_CUT1)
+    # for move in initial.get_moves():
+    #     print(move.map)
+
+    _, goal_state, open_count, closed_count = bfs(initial, None)
+    print(goal_state.map)
+    print(f'open states = {open_count} closed = {closed_count}')
